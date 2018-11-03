137a8002b39ec44953f7e190abc5d5cea71078db
Olga <akulinushkina.olga@gmail.com>
2018-11-03 12:50:58 +0100

    merging

--- dist/app/src/alexa/handlers/recipe.handlers.js
+++ dist/app/src/alexa/handlers/recipe.handlers.js
@@ -120,6 +120,7 @@ module.exports = Alexa.CreateStateHandler(States.RECIPE, {
     
     'AMAZON.YesIntent' : function () {
         console.log('CHEEEEEEEEEEECK');
+        this.handler.state = States.NONE;
         this.response.speak('Lass uns kochen')
             .listen(SpeechOutputUtils.pickRandom(this.t('REPEAT')));
 
