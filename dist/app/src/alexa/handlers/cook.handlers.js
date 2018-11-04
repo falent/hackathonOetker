d4c8717f6566fc67d49c1375559c29aeaffcabe4
falent <you@example.com>
2018-11-04 02:02:23 +0100

    how to render image

--- dist/app/src/alexa/handlers/cook.handlers.js
+++ dist/app/src/alexa/handlers/cook.handlers.js
@@ -108,6 +108,7 @@ module.exports = Alexa.CreateStateHandler(States.COOK, {
 
 
 
+
                                 if (wordInString(sentence, word)){
                                     console.log(sentence);
                                     console.log(word);
